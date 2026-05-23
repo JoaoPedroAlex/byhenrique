6f279a9d5ee0564ea2ee3a507f652f8fbb5aea47
João Pedro Alexandre <joao.pedro.alexandre@gmail.com>
2026-05-23 17:17:32 +0100

    Improve order form menu selection

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,6 +1,6 @@
 export const site = {
   name: "ByHenrique",
-  email: "geral@byhenrique.pt",
+  email: "henriquealexandre.pt@byhenrique.pt",
   /* phoneLabel: "+351 900 000 000",
   phoneHref: "+351900000000",
   whatsappUrl: "https://wa.me/351900000000", */
