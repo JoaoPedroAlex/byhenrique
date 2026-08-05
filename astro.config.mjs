dfa4123cb623022a4151a1b8607ae27f3522f130
JOAO PEDRO ALEXANDRE <joaopedro1@kyndryl.com>
2026-08-05 18:38:15 +0100

    fixing broking page...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://byhenrique.pt',
-  
+  site: "https://byhenrique.pt",
+
+  redirects: {
+    "/en/order": "/en/book/",
+  },
+
   server: {
-  host: true,
-  port: 80,
-},
+    host: true,
+    port: 80,
+  },
 
   vite: {
-  server: {
-    allowedHosts: ['byhenrique.pt'],
+    server: {
+      allowedHosts: ["byhenrique.pt"],
+    },
   },
-},
 
   integrations: [sitemap()],
 });
