db92fe092ef43a74e8524d03299de87d7452103b
JOAO PEDRO ALEXANDRE <joaopedro1@kyndryl.com>
2026-08-06 12:32:29 +0100

    add take a chef reviews

--- src/data/site.ts
+++ src/data/site.ts
@@ -9,4 +9,5 @@ export const site = {
   googleReviewsUrl: "https://www.google.com/search?client=mobilesearchapp&channel=iss&cs=1&hl=en_GB&rlz=1MDAPLA_en-GBPT1020PT1032&sca_esv=440566a5e3a20e4e&v=432.8.954074404&sxsrf=APpeQnt0HcPYzS2de0QopzVAFLL4kyTjVQ:1786015423483&q=By+Henrique+Private+Chef+Algarve&si=APenkKm7iecQ4G6P-TsbSMFKIQtv3EFIqRAFw-i8uEbk55Z-_wsQo-h4O5ofCWaoO7m1sDqJU-G7nvzqtAppR-q_RMwaEWVLf8ori5B-hc38xtQ91fIGe38%3D&uds=AJ5uw18ntM3Q-42NR7dax1P9A6o1Gss7peqLxsdak8ub2QXf1cUR8qTwdkr-BimtHrIR4Qg9cqBPo-tYT2Ze8e4MhDD0bQ6Aor7eWYjbkOtwrnasw1FkHjHg0PWM0fOuaSaW41zd9gbc&sa=X&ved=2ahUKEwivkoT38YuWAxX3T6QEHXBGBUgQ3PALegQIJRAE&biw=1489&bih=828&dpr=1",
   googleReviewsEmbedUrl: "https://www.google.com/maps?q=By%20Henrique%20Private%20Chef%20Algarve&output=embed&hl=en&z=14",
   chefMaisonReviewsUrl: "https://chefmaison.com/en-us/chef/henrique-alexandre/?queryID=9ee6ecb9e10b92cead7ec91031942c71&location=37.130505%2C-8.5935063&address=Alvor%2C+Portugal&guests=6&dates=20-08-2026",
+  takeAChefReviewsUrl: "https://www.takeachef.com/en-pt/chef/henrique-henrique?utm_source=google&utm_medium=organic-seo&utm_content=en-pt-chef-henrique-henrique",
 };
