df9749022fb9537cd74e3a823fa6da7b1b18e803
JOAO PEDRO ALEXANDRE <joaopedro1@kyndryl.com>
2026-08-06 11:10:14 +0100

    adding chefmaison reviews

--- src/data/site.ts
+++ src/data/site.ts
@@ -8,4 +8,5 @@ export const site = {
   instagramUrl: "https://www.instagram.com/byhenrique_alexandre",
   googleReviewsUrl: "https://www.google.com/maps/search/?api=1&query=ByHenrique+Portugal+Chef+Privado",
   googleReviewsEmbedUrl: "https://www.google.com/maps?q=ByHenrique%20Portugal%20Chef%20Privado&output=embed&hl=en&z=14",
+  chefMaisonReviewsUrl: "https://chefmaison.com/en-us/chef/henrique-alexandre/?queryID=9ee6ecb9e10b92cead7ec91031942c71&location=37.130505%2C-8.5935063&address=Alvor%2C+Portugal&guests=6&dates=20-08-2026",
 };
