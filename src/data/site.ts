0256785eb9fe7b22c724183e490dccae602fa229
JOAO PEDRO ALEXANDRE <joaopedro1@kyndryl.com>
2026-08-06 10:44:19 +0100

    adding google reviews

--- src/data/site.ts
+++ src/data/site.ts
@@ -6,6 +6,6 @@ export const site = {
   whatsappUrl: "https://wa.me/351900000000", */
   location: "Portugal",
   instagramUrl: "https://www.instagram.com/byhenrique_alexandre",
-  googleReviewsUrl: "https://www.google.com/maps/search/?api=1&query=ByHenrique+Portugal",
-  googleReviewsEmbedUrl: "https://www.google.com/maps?q=ByHenrique%20Portugal&output=embed&hl=en&z=14",
+  googleReviewsUrl: "https://www.google.com/maps/search/?api=1&query=ByHenrique+Portugal+Chef+Privado",
+  googleReviewsEmbedUrl: "https://www.google.com/maps?q=ByHenrique%20Portugal%20Chef%20Privado&output=embed&hl=en&z=14",
 };
